6974cbee9a424d18f9f785edcbb7ade00d3cf1d8
Ha Lee <halee9@gmail.com>
2026-03-20 10:03:58 -0700

    Revert "fix: add legacy browser support for Samsung signage TV"
    This reverts commit 2e5a46d3efad98018fcf1bfdcafaf377f06f7cd5.

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,17 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import legacy from '@vitejs/plugin-legacy'
 import browserslist from 'browserslist'
 import { browserslistToTargets } from 'lightningcss'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    legacy({
-      targets: ['chrome >= 56'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
     cssMinify: 'lightningcss',
   },
   css: {
     lightningcss: {
-      targets: browserslistToTargets(browserslist('chrome >= 56')),
+      targets: browserslistToTargets(browserslist('chrome >= 80')),
     },
   },
   resolve: {
